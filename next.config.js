4eed0e8e3f62eba4365004b72f16eeb6f43b374c
Trezy <tre@trezy.com>
2020-01-28 21:51:46 -0600

    ci(Now): remove `publicRuntimeConfig`
    `publicRuntimeConfig` is not valid for serverless builds on Now.

--- next.config.js
+++ next.config.js
@@ -8,11 +8,4 @@ const withSass = require('@zeit/next-sass')
 
 
 
-module.exports = withSass(withCSS({
-  publicRuntimeConfig: {
-    TILTIFY_ACCESS_TOKEN: '@tiltify-access-token',
-    TWITCH_ACCESS_TOKEN: '@twitch-access-token',
-    TWITCH_CHANNEL: '@twitch-channel',
-    TWITCH_USERNAME: '@twitch-username',
-  },
-}))
+module.exports = withSass(withCSS())
