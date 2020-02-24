61eeaad889fba0f256ffe326cb28b87bebd5d0f7
Trezy <tre@trezy.com>
2020-02-24 13:29:58 -0600

    feat(/): add a blank slate

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+// Style imports
+import '../scss/reset.scss'
+import '../scss/app.scss'
+
+
+
+
+
+// Module imports
+import React from 'react'
+
+
+
+
+
+const Overlay = () => (
+  <div>Blank slate.</div>
+)
+
+
+
+
+
+export default Overlay
