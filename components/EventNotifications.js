2d8a6b6e732629ee8e65ce4feb45b9ebd03280ec
IanSSenne <IanSSenne@gmail.com>
2020-02-27 02:08:00 -0800

    register existing event types

--- components/EventNotifications.js
+++ components/EventNotifications.js
@@ -1,49 +1,22 @@
 /* eslint-disable react/jsx-no-bind */
 import React from 'react'
 import EventHost from './event-system/EventHost'
-import eventQueue from './event-system'
 import SubscriptionEvent from './SubscriptionEvent'
+import ResubscriptionEvent from './ResubscriptionEvent'
+import BitsEvent from './BitsEvent'
+import HostEvent from './HostEvent'
+import RaidEvent from './RaidEvent'
+import TiltifyDonationEvent from './TiltifyDonationEvent'
 
-EventHost.register('test', props => (<code>{JSON.stringify(props)}</code>))
-EventHost.register('sub', SubscriptionEvent)
+EventHost.register('subscription', SubscriptionEvent)
+EventHost.register('resubscription', ResubscriptionEvent)
+EventHost.register('bits', BitsEvent)
+EventHost.register('host', HostEvent)
+EventHost.register('raid', RaidEvent)
+EventHost.register('tiltify-donation', TiltifyDonationEvent)
 
-let id = 0
-export const EventNotifications = () => {
-  // eslint-disable-next-line no-return-assign
-  const _handleAddTestEventLOW = () => eventQueue.eventQueuePush('test', { duration: 5000, id: (id += 1), priority: 'LOW' }, eventQueue.PRIORITY.LOW)
-  // eslint-disable-next-line no-return-assign
-  const _handleAddTestEventMEDIUM = () => eventQueue.eventQueuePush('test', { duration: 5000, id: (id += 1), priority: 'MEDIUM' }, eventQueue.PRIORITY.MEDIUM)
-  // eslint-disable-next-line no-return-assign
-  const _handleAddTestEventHIGH = () => eventQueue.eventQueuePush('test', { duration: 5000, id: (id += 1), priority: 'HIGH' }, eventQueue.PRIORITY.HIGH)
-
-  const _handleSubEvent = () => eventQueue.eventQueuePush('sub', { displayName: 'Nitro', duration: 5000 }, eventQueue.PRIORITY.MEDIUM_HIGH)
-  return (
-    <div className="event-notifications">
-      <EventHost />
-      <br />
-      <button
-        type="button"
-        onClick={_handleAddTestEventLOW}>
-        {'TEST EVENT LOW PRIORITY'}
-      </button>
-      <br />
-      <button
-        type="button"
-        onClick={_handleAddTestEventHIGH}>
-        {'TEST EVENT HIGH PRIORITY'}
-      </button>
-      <br />
-      <button
-        type="button"
-        onClick={_handleAddTestEventMEDIUM}>
-        {'TEST EVENT MEDIUM PRIORITY'}
-      </button>
-      <br />
-      <button
-        type="button"
-        onClick={_handleSubEvent}>
-        {'SUB EVENT MEDIUM_HIGH PRIORITY'}
-      </button>
-    </div>
-  )
-}
+export const EventNotifications = () => (
+  <div className="event-notifications">
+    <EventHost />
+  </div>
+)
